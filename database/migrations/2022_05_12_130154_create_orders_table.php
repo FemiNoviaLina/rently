631a5eb5ee576569c988459f2a632dc687e585b5
Femi Novia Lina <77434812+FemiNoviaLina@users.noreply.github.com>
2022-05-13 13:32:02 +0700

    update database schema

--- database/migrations/2022_05_12_130154_create_orders_table.php
+++ database/migrations/2022_05_12_130154_create_orders_table.php
@@ -15,6 +15,29 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id')->references('id')->on('users');
+            $table->unsignedBigInteger('vehicle_id')->references('id')->on('vehicles');
+            $table->string('phone_1');
+            $table->string('phone_2');
+            $table->string('address_id');
+            $table->string('address_mlg');
+            $table->time('pickup_time');
+            $table->date('pickup_date');
+            $table->string('pickup_address');
+            $table->time('dropoff_time');
+            $table->date('dropoff_date');
+            $table->string('dropoff_address');
+            $table->string('id_card');
+            $table->string('id_card_2');
+            $table->string('driver_license');
+            $table->float('total_price', 10, 2);
+            $table->string('note');
+            $table->enum('order_status', ['PENDING', 'REJECTED', 'WAITING_FOR_PAYMENT', 'PAYMENT_DONE', 'CANCELED', 'COMPLETED']);
+            $table->string('payment_method')->nullable();
+            $table->string('transaction_id')->nullable();
+            $table->string('virtual_account')->nullable();
+            $table->string('qr_link')->nullable();
+            $table->string('deep_link')->nullable();
             $table->timestamps();
         });
     }
